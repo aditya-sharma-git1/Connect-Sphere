1024eff39e6b63eeff777933bb1a3c03bccc2538
aditya-sharma-git1 <sharma.adityaa111@gmail.com>
2026-06-12 17:22:30 +0530

    Initial Phase Completed

---
+++ frontend/src/pages/landing.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import "../App.css"
+import { Link, useNavigate } from 'react-router-dom'
+export default function LandingPage() {
+
+
+    const router = useNavigate();
+
+    return (
+        <div className='landingPageContainer'>
+            <nav>
+                <div className='navHeader'>
+                    <h2>ConnectSphere</h2>
+                </div>
+                <div className='navlist'>
+                    <p onClick={() => {
+                        router("/aljk23")
+                    }}>Join as Guest</p>
+                    <p onClick={() => {
+                        router("/auth")
+
+                    }}>Register</p>
+                    <div onClick={() => {
+                        router("/auth")
+
+                    }} role='button'>
+                        <p>Login</p>
+                    </div>
+                </div>
+            </nav>
+
+
+            <div style={{ width: "60%" }} className="landingMainContainer">
+                <div>
+                    <h1><span style={{ color: "#FFC300" }}>Connect</span> with your loved Ones</h1>
+
+                    <p>Cover a distance by ConnectSphere</p>
+                    <div role='button'>
+                        <Link to={"/auth"}>Get Started </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
